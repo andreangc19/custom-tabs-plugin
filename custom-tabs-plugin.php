4db3e1a0f5b531a4eba8866396bd6814f282080d
andreangc19 <andreangc19@gmail.com>
2024-06-27 10:06:38 +0300

    Scaffolding the Custom tabs plugin

---
+++ custom-tabs-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Custom Tabs Plugin
+ * Plugin URI:      https://provisional.com
+ * Description:     Adds custom tabs to any page on your website
+ * Author:          Andrea GV
+ * Author URI:      https://sirius-web.net
+ * Text Domain:     custom-tabs-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Custom_Tabs_Plugin
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
